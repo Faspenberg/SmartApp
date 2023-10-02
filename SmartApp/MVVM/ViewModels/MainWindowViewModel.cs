21e1d0fbf2e021fe74bd371daea9e93ac8a59a25
Fredrik Aspenberg <aspenberg16@hotmail.com>
2023-10-02 11:21:24 +0200

    ADDED: Services

---
+++ SmartApp/MVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartApp.MVVM.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public MainWindowViewModel(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            CurrentViewModel = _serviceProvider.GetRequiredService<HomeViewModel>();
+        
+        }
+
+        [ObservableProperty]
+
+        private ObservableObject? _currentViewModel;
+
+    }
+}
